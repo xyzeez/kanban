f2fd9504159a5f010d23e842888be0a4ff71d3f7
Abdu-Samii Abdul-Azeez <106898953+xyzeez@users.noreply.github.com>
2025-02-01 05:27:27 +0300

    ui(app): modified button hover style for enabled buttons

--- src/ui/placeholders/EmptyState.tsx
+++ src/ui/placeholders/EmptyState.tsx
@@ -19,7 +19,7 @@ const EmptyState: FC<EmptyStateProps> = ({ type }) => {
   return (
     <div className="flex h-full flex-col items-center justify-center p-4">
       <p className="text-gray-400 mb-4">{content[type].message}</p>
-      <button className="bg-purple-500 hover:bg-purple-600 rounded-lg px-4 py-2">
+      <button className="bg-purple-500 enabled:hover:bg-purple-600 rounded-lg px-4 py-2">
         {content[type].action}
       </button>
     </div>
